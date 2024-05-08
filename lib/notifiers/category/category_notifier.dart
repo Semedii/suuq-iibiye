6673d8c929570ecc58e675e305ebfa29eb78873d
semedii <abdisamed2023@hotmail.com>
2024-05-08 22:24:59 +0300

    compresses file

--- lib/notifiers/category/category_notifier.dart
+++ lib/notifiers/category/category_notifier.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
+import 'package:image/image.dart' as img;
 
 import 'package:image_picker/image_picker.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -49,7 +51,9 @@ class CategoryNotifier extends _$CategoryNotifier {
     List<String> encodedImages = [];
     for (XFile? file in files) {
       if (file != null) {
-        Uint8List image = await file.readAsBytes();
+       File compressedFile =  await compressAndResizeImage(file);
+       XFile compressedXfile = XFile(compressedFile.path);
+        Uint8List image = await compressedXfile.readAsBytes();
         String encodedImage = base64Encode(image);
         encodedImages.add(encodedImage);
       }
@@ -64,4 +68,33 @@ class CategoryNotifier extends _$CategoryNotifier {
     await ProductDataService().deleteProduct(productId: productId, category: lastState.category);
     await initPage(lastState.category);
   }
+
+  Future<File> compressAndResizeImage(XFile fille) async{
+    File file = File(fille.path);
+
+  img.Image image = img.decodeImage(file.readAsBytesSync())!;
+
+  // Resize the image to have the longer side be 800 pixels
+  int width;
+  int height;
+
+  if (image.width > image.height) {
+    width = 800;
+    height = (image.height / image.width * 800).round();
+  } else {
+    height = 800;
+    width = (image.width / image.height * 800).round();
+  }
+
+  img.Image resizedImage = img.copyResize(image, width: width, height: height);
+
+  // Compress the image with JPEG format
+  List<int> compressedBytes = img.encodeJpg(resizedImage, quality: 75);  // Adjust quality as needed
+
+  // Save the compressed image to a file
+  File compressedFile = File(file.path.replaceFirst('.jpg', '_compressed.jpg'));
+  compressedFile.writeAsBytesSync(compressedBytes);
+
+  return compressedFile;
+}
 }
