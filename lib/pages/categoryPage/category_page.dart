bf35981365286e3b4b7d2d3bf7a8bc3c0c9053ac
semedii <abdisamed2023@hotmail.com>
2024-05-29 16:51:35 +0300

    remove imports

--- lib/pages/categoryPage/category_page.dart
+++ lib/pages/categoryPage/category_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:suuq_iibiye/components/app_button.dart';
 import 'package:suuq_iibiye/components/product_card.dart';
 import 'package:suuq_iibiye/models/product.dart';
