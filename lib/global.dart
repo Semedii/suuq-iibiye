185a825f6ca2560b8ae859a3ca21632f36d69724
semedii <abdisamed2023@hotmail.com>
2024-06-29 10:23:39 +0300

    final env config

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
-import 'package:suuq_iibiye/firebase_options.dart';
 import 'package:suuq_iibiye/services/storage_service.dart';
 
 class Global {
@@ -8,9 +6,6 @@ class Global {
 
   static init() async {
     WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
     storageService = await StorageService().init();
   }
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,10 +1,14 @@
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:suuq_iibiye/firebase_options_prod.dart';
 import 'package:suuq_iibiye/global.dart';
 import 'package:suuq_iibiye/my_app.dart';
 
 void main() async {
   await Global.init();
-  runApp(ProviderScope(child: MyApp(env:  "prod")));
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(ProviderScope(child: MyApp(env: "prod")));
 }
